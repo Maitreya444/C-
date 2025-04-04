6e688b3377bc35cb3603beff6cee92c0999b5cfe
Maitreya Gangurde <94222925+Maitreya444@users.noreply.github.com>
2025-04-04 14:39:41 +0530

    Add files via upload

---
+++ EF/EntityFrameworkCore.Data/FootballDBcontext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using EntityFrameworkCore.Domain;
+
+namespace EntityFrameworkCore.Data
+{
+    public class FootballDBContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("YourConnectionStringHere");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ EF/EntityFrameworkCore.Domain/Coach.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EntityFrameworkCore.Domain
+{   
+    public abstract class BaseDomainModel
+    {
+        public DateTime CreateDate { get; set; }
+    }
+
+    public class Coach : BaseDomainModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } //string is like varchar
+    }
+
+    public class Team : BaseDomainModel
+    {
+        public int TeamID { get; set; }
+        public string? Name { get; set; } //? means it is a nullable db
+    }
+
+}
+
